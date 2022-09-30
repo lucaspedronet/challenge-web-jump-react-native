35764c5b8800e7ecc442ed71214a64fae3833bcc
Lucas Pedro <lucaspedrolbg@gmail.com>
2022-09-30 08:00:00 -0300

    fix: config ios

---
+++ ios/Pods/Target Support Files/Pods-ChallengeJump-ChallengeJumpTests/Pods-ChallengeJump-ChallengeJumpTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ChallengeJump_ChallengeJumpTests : NSObject
+@end
+@implementation PodsDummy_Pods_ChallengeJump_ChallengeJumpTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-ChallengeJump/Pods-ChallengeJump-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ChallengeJump : NSObject
+@end
+@implementation PodsDummy_Pods_ChallengeJump
+@end
