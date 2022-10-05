99be10c8212bf48cf4cbe86f9486154c11de245d
Lucas Pedro <lucaspedrolbg@gmail.com>
2022-10-05 18:06:48 -0300

    feat: add testes do redux e thunck

--- src/screens/public/HomeScreen/HomeScreen.test.tsx
+++ src/screens/public/HomeScreen/HomeScreen.test.tsx
@@ -1,11 +1,44 @@
 import React from 'react';
+import thunk from 'redux-thunk';
+import configureStore from 'redux-mock-store'
 import { render } from '@testing-library/react-native';
-import { HomeScreen } from './HomeScreen';
+import { ThemeProvider } from 'styled-components/native';
+import { Middleware, Dispatch, AnyAction } from 'redux';
 
-const wrapper = (props) => render(<HomeScreen {...props} />);
+import { theme } from '~config/theme';
+import { HomeScreen,  } from './HomeScreen';
+import { filmsPopularAsync } from '../../../store/films/thunks';
+
+
+const Providers: React.FC = ({ children }) => (
+  <ThemeProvider theme={theme}>{children}</ThemeProvider>
+);
+
+const wrapper = (props) => render(
+  <HomeScreen {...props} />,
+  {wrapper: Providers}
+);
+
+const middlewares: Middleware<{}, any, Dispatch<AnyAction>>[] | undefined = [thunk]
+const mockStore = configureStore(middlewares);
 
 test('renderização padrão', () => {
-  const { container } = wrapper({});
-
-  expect(container).toMatchSnapshot();
+  const component = wrapper({})
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
+
+test('chama dispatch fetchByPopularFilms', async () => {
+  const mockInitialState = {
+    films: [],
+    loading: false,
+  };
+
+  const store = mockStore(mockInitialState);
+  await store.dispatch(filmsPopularAsync() as any);
+  const actions = store.getActions();
+
+  expect(actions).toHaveLength(2);
+  expect(actions[0].type).toEqual("films/fetchByPopularFilms/pending");
+  expect(actions[1].payload).toBeTruthy();
+})
